64cb7481cca1902dd54bd1327aea64ec9525620a
saini-sir <sainigsir@gmail.com>
2024-02-02 17:59:11 +0530

    first jenkins testing

---
+++ main.java
@@ -0,0 +1,16 @@
+
+import java.util.Date;
+
+public class main{
+ 
+public static void main(String [] args){
+    System.out.println("this is a java program");
+    System.out.println("this program for jenkins");
+    System.out.println(new Date());
+
+}
+
+
+
+
+}
